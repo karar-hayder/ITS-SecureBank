379f7feaeb1964d99ffdee2169fc02ecbe41a26d
AliLap <hisvdibru@gmail.com>
2026-02-06 13:46:10 +0300

    add auth controller

---
+++ API/Controller/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using API.Common;
+using Microsoft.AspNetCore.Mvc;
+using Application.Dtos;
+namespace API.Controller
+{
+    
+    public class AuthController() : BaseController
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto request)
+        {
+            var result = await authService.RegisterAsync(request);
+            if (!result.Success) return BadRequest(result.Message);
+            return StatusCode(201, result.Data);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto request)
+        {
+            var result = await authService.LoginAsync(request);
+            if (!result.Success) return Unauthorized(result.Message);
+            return Ok(result.Data);
+        }
+        [HttpPost("refresh")]
+        public async Task<IActionResult> RefreshToken(string refreshToken)
+        {
+
+            var result = await authService.RefreshToken(refreshToken);
+
+            return result.Success ? Ok(result.Data) : Unauthorized(result.Message);
+        }
+
+    }
+}
